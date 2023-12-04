2aacebebf272f96370134edb29d84e64783d08b5
stopAngin <stop@angin.com>
2023-12-04 14:20:42 +0100

    .

--- src/main/java/sk/stopangin/oauth2server/Oauth2serverApplication.java
+++ src/main/java/sk/stopangin/oauth2server/Oauth2serverApplication.java
@@ -2,6 +2,12 @@ package sk.stopangin.oauth2server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 
 @SpringBootApplication
 public class Oauth2serverApplication {
@@ -10,4 +16,16 @@ public class Oauth2serverApplication {
     SpringApplication.run(Oauth2serverApplication.class, args);
   }
 
+  @Bean
+  public RegisteredClientRepository registeredClientRepository() {
+    RegisteredClient registeredClient = RegisteredClient.withId("admin")
+        .clientId("admin")
+        .clientSecret("{noop}password23")
+        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+        .scope("kafka")
+        .build();
+
+    return new InMemoryRegisteredClientRepository(registeredClient);
+  }
 }
