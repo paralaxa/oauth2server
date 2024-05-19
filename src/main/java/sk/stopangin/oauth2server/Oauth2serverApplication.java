9135bb4f0493bce830f9b629446d12df4ca5dfa6
stopAngin <stop@angin.com>
2024-05-19 20:10:37 +0200

    .

--- src/main/java/sk/stopangin/oauth2server/Oauth2serverApplication.java
+++ src/main/java/sk/stopangin/oauth2server/Oauth2serverApplication.java
@@ -1,14 +1,24 @@
 package sk.stopangin.oauth2server;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @SpringBootApplication
 public class Oauth2serverApplication {
 
@@ -17,15 +27,39 @@ public class Oauth2serverApplication {
   }
 
   @Bean
-  public RegisteredClientRepository registeredClientRepository() {
-    RegisteredClient registeredClient = RegisteredClient.withId("admin")
-        .clientId("admin")
-        .clientSecret("{noop}password23")
-        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-        .scope("kafka")
+  @Order(1)
+  SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+    OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+    http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+        .oidc(withDefaults()); // Enable OpenID Connect 1.0
+    return http.formLogin(withDefaults()).build();
+  }
+
+  @Bean
+  @Order(2)
+  SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest()
+            .authenticated())
+        .formLogin(withDefaults());
+    return http.build();
+  }
+
+  @Bean
+  public UserDetailsService users() {
+    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    UserDetails user = User.builder()
+        .username("user")
+        .password("password")
+        .passwordEncoder(encoder::encode)
+        .roles("USER")
         .build();
 
-    return new InMemoryRegisteredClientRepository(registeredClient);
+    UserDetails admin = User.builder()
+        .username("admin")
+        .password("password")
+        .passwordEncoder(encoder::encode)
+        .roles("ADMIN")
+        .build();
+    return new InMemoryUserDetailsManager(user, admin);
   }
 }
