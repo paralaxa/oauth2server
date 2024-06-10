be5ff03c4dca244be5538af3284277507270694c
stopAngin <stop@angin.com>
2024-06-10 20:41:00 +0200

    .

--- src/main/java/sk/stopangin/oauth2server/Oauth2serverApplication.java
+++ src/main/java/sk/stopangin/oauth2server/Oauth2serverApplication.java
@@ -2,19 +2,26 @@ package sk.stopangin.oauth2server;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -61,5 +68,22 @@ public class Oauth2serverApplication {
         .roles("ADMIN")
         .build();
     return new InMemoryUserDetailsManager(user, admin);
+  }
+
+  @Bean
+  public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer() {
+    return context -> {
+      if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
+        context.getClaims().claims(claims -> {
+          Set<String> roles = AuthorityUtils.authorityListToSet(
+                  context.getPrincipal().getAuthorities())
+              .stream()
+              .map(c -> c.replaceFirst("^ROLE_", ""))
+              .collect(
+                  Collectors.collectingAndThen(Collectors.toSet(), Collections::unmodifiableSet));
+          claims.put("roles", roles);
+        });
+      }
+    };
   }
 }
